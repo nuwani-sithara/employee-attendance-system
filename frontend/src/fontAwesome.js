fe0f591022bafa2fe12baca439417b54dae61d2b
nuwani-sithara <it22099518@my.sliit.lk>
2025-07-22 21:59:51 +0530

    added font awsome for icons

---
+++ frontend/src/fontAwesome.js
@@ -0,0 +1,41 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { 
+  faArrowLeft, 
+  faMapMarkedAlt,
+  faFingerprint,
+  faRightFromBracket,
+  faSignInAlt,
+  faSignOutAlt,
+  faUser,
+  faLock,
+  faSpinner,
+  faClipboard,
+  faSearch,
+  faCircleCheck,
+  faCircleXmark,
+  faRotate,
+  faCircleExclamation,
+  faUserTag
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(
+  faArrowLeft,
+  faMapMarkedAlt,
+  faFingerprint,
+  faRightFromBracket,
+  faSignInAlt,
+  faSignOutAlt,
+  faUser,
+  faLock,
+  faSpinner,
+  faClipboard,
+  faSearch,
+  faCircleCheck,
+  faCircleXmark,
+  faRotate,
+  faCircleExclamation,
+  faUserTag
+);
+
+export { FontAwesomeIcon };
